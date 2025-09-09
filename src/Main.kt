555620b614d5a4aa9a0d28fd0d3e49b044861d6d
10230938 <ml2676117@gmail,com>
2025-09-09 12:13:51 +0300

    1 задание

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main(){
+    print("Введите строку ")
+    val test = readln()
+    val testCount = test.groupingBy { it }.eachCount()
+    println("")
+    testCount.forEach { (test, count) -> print("$test$count") }
 }
+
+//Создать приложение, которое подсчитывает
+// количество подряд идущих одинаковых символов
+// во введенной строке. На вход подается, например,
+// строка AAADSSSRRTTHAAAA. На выходе получаем
+// A3DS3R2T2HA4. То есть, если количество подряд идущих
+// символов меньше двух, то мы не пишем единицу
+
+
